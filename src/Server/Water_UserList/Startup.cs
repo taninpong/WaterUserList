bdd89f819ad14887f051baf84bef9883cc333414
parada <parada@perfenterprise.com>
2019-03-06 16:43:45 +0700

    Add swagger.

--- src/Server/Water_UserList/Startup.cs
+++ src/Server/Water_UserList/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Water_UserList
 {
@@ -26,6 +27,10 @@ namespace Water_UserList
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
             services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddMvc().AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
@@ -43,6 +48,8 @@ namespace Water_UserList
                 app.UseHsts();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"); });
             app.UseCors(builder =>
                 builder.WithOrigins("*")
                 .AllowAnyMethod()
